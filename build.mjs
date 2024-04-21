7f8695a8e797558add97632d0b61c2a4e43a9907
Luiz Ferraz <luiz.ferraz@croct.com>
2024-04-21 20:42:31 -0300

    Build using esbuild

---
+++ build.mjs
@@ -0,0 +1,15 @@
+import * as esbuild from 'esbuild';
+
+await esbuild.build({
+    entryPoints: ['src/index.ts'],
+    bundle: true,
+    minify: false,
+    outfile: 'build/index.js',
+    platform: 'neutral',
+    format: 'esm',
+    sourcemap: 'external',
+    external: [
+        'nanostores',
+        '@croct/plug',
+    ],
+});
