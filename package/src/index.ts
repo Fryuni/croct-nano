4e7534244dd61d8439eb7f90f489604ccafb55de
Luiz Ferraz <luiz.ferraz@croct.com>
2026-02-16 10:06:13 -0300

    chore: refactor croct bindings (#23)

--- package/src/index.ts
+++ package/src/index.ts
@@ -1,6 +1,6 @@
 import './croctPlugin.js';
-import croct from '@croct/plug';
 import { type CroctAtom, croctContent } from './croctAtom.js';
+import { croct } from './common.js';
 export { trackCart, trackSessionField, trackUserField } from './autoPatching.js';
 
 export { croct, croctContent, type CroctAtom };
