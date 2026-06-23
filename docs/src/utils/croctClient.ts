858cfd9ea764b60f6cc75108d5e8575b4a4544e3
Luiz Ferraz <luiz@lferraz.com>
2026-06-23 01:03:07 +0000

    Set Croct ID

--- docs/src/utils/croctClient.ts
+++ docs/src/utils/croctClient.ts
@@ -2,10 +2,15 @@ import { croct } from 'croct-nanostores';
 import { CROCT_APP_ID } from 'astro:env/client';
 import type { GlobalPlug } from '@croct/plug/plug';
 
+const clientId = localStorage.getItem('croct.cid') ?? crypto.randomUUID().replaceAll('-', '');
+
+localStorage.setItem('croct.cid', clientId);
+
 croct.plug({
     appId: CROCT_APP_ID,
     debug: import.meta.env.DEV,
     token: null,
+    clientId,
     disableCidMirroring: true,
     plugins: { 'auto-refresh-atom': true },
 });
